691c56f024a4c5f4623f41a097f1af1ff7ddaf8c
dat.tran <dat.tran@saigontechnology.vn>
2018-09-21 11:14:40 +0700

    support on Android

--- README.md
+++ README.md
@@ -20,10 +20,24 @@ Only work on iOS
 3. In XCode, in the project navigator, select your project. Add `libRNReactNativeJsBackgroundThread.a` to your project's `Build Phases` ➜ `Link Binary With Libraries`
 4. Run your project (`Cmd+R`)<
 
+#### Android
+
+1. Open up `android/app/src/main/java/[...]/MainActivity.java`
+  - Add `import com.reactlibrary.RNReactNativeJsBackgroundThreadPackage;` to the imports at the top of the file
+  - Add `new RNReactNativeJsBackgroundThreadPackage()` to the list returned by the `getPackages()` method
+2. Append the following lines to `android/settings.gradle`:
+  	```
+  	include ':react-native-js-background-thread'
+  	project(':react-native-js-background-thread').projectDir = new File(rootProject.projectDir, 	'../node_modules/react-native-js-background-thread/android')
+  	```
+3. Insert the following lines inside the dependencies block in `android/app/build.gradle`:
+  	```
+      compile project(':react-native-js-background-thread')
+  	```
 
 ## Usage
 ```javascript
-import {runAsync} from 'react-native-js-background-thread';
+    import {runAsync} from 'react-native-js-background-thread';
 
     runMe(a, b) {
         let count = 0
@@ -36,8 +50,8 @@ import {runAsync} from 'react-native-js-background-thread';
         return JSON.stringify(count)
     }
 
-  runAsync(this.runMe, 'runMe', [200000, 10000])
-    .then(result => {
-      console.log('output: ', result)
-    })
+    runAsync(this.runMe, 'runMe', [200000, 10000])
+        .then(result => {
+          console.log('output: ', result)
+        })
   
--- android/src/main/java/com/reactlibrary/RNReactNativeJsBackgroundThreadModule.java
+++ android/src/main/java/com/reactlibrary/RNReactNativeJsBackgroundThreadModule.java
@@ -1,22 +1,44 @@
 
 package com.reactlibrary;
 
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReadableArray;
+
+import org.liquidplayer.javascript.JSContext;
+import org.liquidplayer.javascript.JSFunction;
+import org.liquidplayer.javascript.JSValue;
 
 public class RNReactNativeJsBackgroundThreadModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
+    private final ReactApplicationContext reactContext;
+    JSContext jsContext;
 
-  public RNReactNativeJsBackgroundThreadModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
+    public RNReactNativeJsBackgroundThreadModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+        jsContext = new JSContext();
+    }
 
-  @Override
-  public String getName() {
-    return "RNReactNativeJsBackgroundThread";
-  }
+    @Override
+    public String getName() {
+        return "RNReactNativeJsBackgroundThread";
+    }
+
+    @ReactMethod
+    public void runAsyncCode(final String func, final String funcName, final ReadableArray args, final Promise promise) {
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                jsContext.evaluateScript(func);
+                JSFunction function = jsContext.property(funcName).toFunction();
+                JSValue value = function.call(null, args.toArrayList().toArray());
+                promise.resolve(value.toString());
+            }
+        };
+
+        new Thread(r).start();
+    }
 }
